01bd7b0548bd8fd12a88fde16bf637dbf5fd1295
Deepak V <85978656+SeriesZone101@users.noreply.github.com>
2021-07-07 19:50:24 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -54,5 +54,5 @@ For More Details Click Help Button Below..
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/SeriesZone101/Adv-Filter-Szone">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a href="https://t.me/DeepakV111">Click Me</a></i>
 """
